df6f3201c1f32aa0e59a61a8ab40df0c18dfde28
judbrito <admoceano@gmail.com>
2023-08-18 16:32:27 -0300

    Projeto real

---
+++ src/main/java/rest_core/Constantes.java
@@ -0,0 +1,12 @@
+package rest_core;
+
+import io.restassured.http.ContentType;
+
+public interface Constantes {
+
+	String APP_BASE_URL = "https://seubarriga.wcaquino.me/";
+	Integer APP_PORT = 443; // port 80 somente para http sem "https"
+	String APP_BASE_PATH = "";
+	ContentType APP_CONTENT_TYPE = ContentType.JSON;
+	Long MAX_TIMEOUT = 1000L;
+}
