f6a66de73edab7e5f8d6303529c4fdfcea3c3a59
judbrito <admoceano@gmail.com>
2023-08-21 15:05:41 -0300

    aula 55 e 56

--- src/main/java/test/BarrigaTest.java
+++ src/main/java/test/BarrigaTest.java
@@ -1,18 +1,67 @@
 package test;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Before;
 import org.junit.Test;
 
-import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import rest_core.BaseTest;
 
 public class BarrigaTest extends BaseTest {
+	private String TOKEN;
+	
+	@Before
+	public void logar() {
+		Map<String, String> login = new HashMap<String, String>();
+		login.put("email", "oxe@oxe");
+		login.put("senha", "oxeoxe");
+
+		 TOKEN = 
+				given().log().all()
+				.body(login)
+				.contentType(ContentType.JSON)
+				.when().post("/signin")
+				.then()
+				.log()
+				.all()
+				.statusCode(200)
+				.extract().path("token");
+	}
 
 	@Test
 	public void naoDeveacessarAPIsemToken() {
-		RestAssured
+		given().when().get("/contas").then().statusCode(401);
+	}
+
+	@Test
+	public void deveIncluirContaComSucesso() {
+			given()
+		.header("Authorization", "JWT " + TOKEN)
+		.body("{ \"nome\": \"judbrito3\" }")// aqui vc cria uma conta nova 
 		.when()
-		.get("/contas")
+		.post("/contas")
 		.then()
-		.statusCode(200);
+		.statusCode(201);
+
+	}
+	@Test
+	public void deveAlterarContaComSucesso() {
+			given()
+		.header("Authorization", "JWT " + TOKEN)
+		.body("{ \"nome\": \"Alterar Conta\" }")
+		.when()
+		.put("/contas/1865357")
+		.then()
+		.log()
+		.all()
+		.statusCode(200)
+		.body("nome", is("Alterar Conta"));
+
 	}
 }
+
